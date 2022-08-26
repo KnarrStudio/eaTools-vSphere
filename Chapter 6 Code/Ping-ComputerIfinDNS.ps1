26f95fe0f123ed5982b670355670822509d87abb
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-08-25 22:10:04 -0500

    Create Ping-ComputerIfinDNS.ps1

---
+++ Chapter 6 Code/Ping-ComputerIfinDNS.ps1
@@ -0,0 +1,30 @@
+﻿param(
+$serverName = 'buffalo'
+)
+
+$ipaddress = @{
+  IPAddress = $false
+}
+
+try
+{
+  $ipaddress = Resolve-DnsName -Name $serverName -Type A -ErrorAction Stop
+}
+catch
+{
+  $ipaddress.IPAddress = $false
+}
+
+if($ipaddress.IPAddress)
+{
+  $pingresult = Test-Connection -ComputerName $ipaddress.IPAddress -Count 1 -Quiet
+  if($pingresult -eq $true)
+  {
+    Write-Host -Object $true
+  }
+}
+else
+{
+  Write-Warning -Message $false
+}
+
